eb360563951ce41a1722b5a42ccc25668919176d
khushbu-ty <khushbuameta1991@gmail.com>
2020-04-24 17:38:16 +0530

    Adding Interface for making Variable as final

--- src/test/java/com/rupeek/customaersearch/tests/GetAllRecordTest.java
+++ src/test/java/com/rupeek/customaersearch/tests/GetAllRecordTest.java
@@ -6,28 +6,29 @@ import static org.hamcrest.Matchers.lessThan;
 import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
 
+import customersearch_app.IAutho;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
-public class GetAllRecordTest {
+public class GetAllRecordTest implements IAutho {
 	
 	
 	@Test
 	public void getAutherizationWithValid()
 	{
-   given().header("Authorization","Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJydXBlZWsiLCJleHAiOjE1ODc3MzgxNDAsImlhdCI6MTU4NzcyMDE0MH0.KdCLd7wsDA8CxX3OROxRXsVVk9gnkzYREsk89VZl9l_OvhQKqDHFgcdajuLb2i9TBi_vI6A4TmvJhkOO87zfRg").get("http://13.126.80.194:8080/api/v1/users")
+   given().header("Authorization",TOKEN).get("http://13.126.80.194:8080/api/v1/users")
    .then().assertThat().statusCode(200).and().contentType(ContentType.JSON).and()
    .body("first_name", Matchers.hasItem("Aliko")).and().body("last_name", Matchers.hasItem("Dangote")).and()
    .body("first_name", Matchers.hasItem("Bill")).and().body("last_name", Matchers.hasItem("Gates")).and()
    .body("first_name", Matchers.hasItem("Folrunsho")).and().body("last_name", Matchers.hasItem("Alakija"));
    
-		
+	
 		
 	}
 	@Test
 	public void getAutherizationWithInValid()
 	{
-   given().header("Authorization","Bearer eyJhbGciOiJIUzUxMiertJ9.eyJzdWIiOiJydXBlZWsiLCJleHAiOjE1ODc3MzgxNDAsImlhdCI6MTU4NzcyMDE0MH0.KdCLd7wsDA8CxX3OROxRXsVVk9gnkzYREsk89VZl9l_OvhQKqDHFgcdajuLb2i9TBi_vI6A4TmvJhkOO87zfRg").get("http://13.126.80.194:8080/api/v1/users")
+   given().header("Authorization","WRONG_TOKEN").get("http://13.126.80.194:8080/api/v1/users")
    .then().assertThat().statusCode(401).and().contentType("");
   
 		
--- src/test/java/com/rupeek/customaersearch/tests/GetSingleRecordByPhone.java
+++ src/test/java/com/rupeek/customaersearch/tests/GetSingleRecordByPhone.java
@@ -6,15 +6,16 @@ import static io.restassured.RestAssured.given;
 import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
 
+import customersearch_app.IAutho;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
-public class GetSingleRecordByPhone {
+public class GetSingleRecordByPhone implements IAutho {
 	
 	@Test
 	public void getAutherizationWithValidphone()
 	{
-   given().header("Authorization","Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJydXBlZWsiLCJleHAiOjE1ODc3MzgxNDAsImlhdCI6MTU4NzcyMDE0MH0.KdCLd7wsDA8CxX3OROxRXsVVk9gnkzYREsk89VZl9l_OvhQKqDHFgcdajuLb2i9TBi_vI6A4TmvJhkOO87zfRg").get("http://13.126.80.194:8080/api/v1/users/8037602400")
+   given().header("Authorization",TOKEN).get("http://13.126.80.194:8080/api/v1/users/8037602400")
    .then().assertThat().statusCode(200).and().contentType(ContentType.JSON).and()
    .body("first_name", Matchers.equalTo("Aliko")).and().body("last_name", Matchers.equalTo("Dangote"));
    
@@ -25,7 +26,7 @@ public class GetSingleRecordByPhone {
 	@Test
 	public void getAutherizationWithInValidphone()
 	{
-   given().header("Authorization","Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJydXBlZWsiLCJleHAiOjE1ODc3MzgxNDAsImlhdCI6MTU4NzcyMDE0MH0.KdCLd7wsDA8CxX3OROxRXsVVk9gnkzYREsk89VZl9l_OvhQKqDHFgcdajuLb2i9TBi_vI6A4TmvJhkOO87zfRg").get("http://13.126.80.194:8080/api/v1/users/8037602467")
+   given().header("Authorization",TOKEN).get("http://13.126.80.194:8080/api/v1/users/8037602467")
    .then().assertThat().statusCode(200).and().contentType("");
    
 		
